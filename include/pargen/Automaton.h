d098fce4bdb79a9131f3838fe16ff359c2c7187e
Мельников Вадим Сергеевич <vasemelnikov@edu.hse.ru>
2025-03-22 23:46:08 +0300

    add Automaton.h docs

--- include/pargen/Automaton.h
+++ include/pargen/Automaton.h
@@ -1,3 +1,10 @@
+/**
+ * @file Automaton.h
+ * @brief Provides a class for interacting with the automaton based on the
+ * formal grammar.
+ * @author Vadim Melnikov
+ * @version 1.0
+ */
 #pragma once
 
 #include <boost/bimap.hpp>
@@ -8,13 +15,42 @@
 #include "Entities.h"
 #include "GrammarAnalyzer.h"
 
+/**
+ * @class Automaton
+ * @brief Represents an automaton, built by the production of the provided
+ * formal grammar.
+ * @details To be specific, the automaton is a deterministic pushdown one
+ * (DPDA). Its nodes, states, are sets of items. Each item is a partially parsed
+ * rule and a lookahead token. Transitions are computed based on the next token.
+ */
 class Automaton {
 public:
+    /**
+     * @struct Item
+     * @brief Represents a single item in the automaton state.
+     */
     struct Item {
+        /**
+         * @brief Constructs an Item object.
+         * @param rule_number The number of the rule in the grammar.
+         * @param dot_pos The position of the dot (pointing at the next unparsed
+         * token) in the rule.
+         * @param lookahead The lookahead token.
+         */
         Item(size_t rule_number, size_t dot_pos, Terminal lookahead);
 
+        /**
+         * @brief The number of the rule in the grammar.
+         */
         size_t rule_number_;
+        /**
+         * @brief The position of the dot (pointing at the next unparsed token)
+         * in the rule.
+         */
         size_t dot_pos_;
+        /**
+         * @brief The lookahead token.
+         */
         Terminal lookahead_;
 
         friend bool operator<(const Item &lhs, const Item &rhs) {
@@ -23,22 +59,67 @@ public:
         }
     };
 
+    /**
+     * @brief An alias for a set of items, representing a state of the
+     * automaton.
+     */
     using State = std::set<Item>;
 
+    /**
+     * @brief An alias for a bidirectional map of states and their numbers.
+     */
     using StateMap = boost::bimap<size_t, State>;
 
+    /**
+     * @brief Constructs an Automaton object from Grammar and GrammarAnalyzer.
+     * @param g The grammar.
+     * @param ga The grammar analyzer.
+     */
     Automaton(const Grammar &g, const GrammarAnalyzer &ga);
 
+    /**
+     * @brief Computes a closure of an arbitrary set of items.
+     * @param items The set of items to compute the closure of.
+     * @return The closure of the given set of items.
+     * @note The closure doesn't have to be a state of the automaton.
+     */
     State Closure(const State &items) const;
+    /**
+     * @brief Computes the next state of the automaton based on the current
+     * state and the next token.
+     * @param state The state to go to from.
+     * @param next The next token.
+     * @return The next state of the automaton.
+     */
     State Goto(const State &state, Token next) const;
 
+    /**
+     * @brief Returns the states of the automaton.
+     * @return The states of the automaton.
+     */
     const StateMap &GetStates() const;
 
+    /**
+     * @brief Returns the next token (if exists).
+     * @param item The item to get the next token from.
+     * @return `std::nullopt` if the dot is at the end of the item, the next
+     * token otherwise.
+     */
     std::optional<Token> NextToken(const Item &item) const;
 
 private:
+    /**
+     * @brief Helper function to determine whether the dot is at the end of the
+     * given item.
+     * @param item The item to check the dot position for.
+     * @return `true` if the dot is at the end of the item, `false` otherwise.
+     */
     bool DotAtEnd(const Item &item) const;
 
+    /**
+     * @brief Computes the canonical collection (all possible states of the
+     * automaton) for the grammar.
+     */
     void BuildCanonicalCollection();
 
     const Grammar &g_;
