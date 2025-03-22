76ad29f288607b4943c2c4c769db226f3cd229c0
Мельников Вадим Сергеевич <vasemelnikov@edu.hse.ru>
2025-03-23 00:04:48 +0300

    add CodeGenerator.h docs

--- include/codegen/CodeGenerator.h
+++ include/codegen/CodeGenerator.h
@@ -1,24 +1,63 @@
+/**
+ * @file CodeGenerator.h
+ * @brief Provides a class for generating all code for the parser.
+ * @author Vadim Melnikov
+ * @version 1.0
+ */
 #include <cstdlib>
 #include <cstring>
 
 #include "Entities.h"
 
+/**
+ * @class CodeGeneratorError
+ * @brief Exception class for reporting errors in the process of generating code
+ * for the parser.
+ */
 class CodeGeneratorError : public std::exception {
 public:
+    /**
+     * @brief Constructs a CodeGeneratorError object with the specified error.
+     */
     explicit CodeGeneratorError(const std::string &msg);
+
+    /**
+     * @brief Returns the error message.
+     * @return The error message.
+     */
     const char *what() const noexcept override;
 
 private:
     std::string msg_;
 };
 
+/**
+ * @class CodeGenerator
+ * @brief Generates all code for the parser.
+ */
 class CodeGenerator {
 public:
+    /**
+     * @brief Constructs a CodeGenerator object with the specified parameters.
+     * @param folder The folder to generate the code to.
+     * @param at The action table to use for the generated code
+     * @param gt The goto table to use for the generated code.
+     * @param fs The FOLLOW sets to use for the generated code.
+     * @param g The grammar to generate the code for.
+     * @param add_json_generator Whether to add a JSON parse tree generator to
+     * the generated parser.
+     * @param json_indents The number of indents to use for the JSON parse tree
+     * (if it is generated).
+     */
     CodeGenerator(
         const std::string &folder, ActionTable &at, GotoTable &gt,
         FollowSets &fs, const Grammar &g, bool add_json_generator,
         size_t json_indents
     );
+
+    /**
+     * @brief Generates all code for the parser.
+     */
     void Generate();
 
 private:
