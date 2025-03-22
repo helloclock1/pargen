36b7c0b3ec113386375ea3778467c39acd9b53f6
Мельников Вадим Сергеевич <vasemelnikov@edu.hse.ru>
2025-03-22 23:59:36 +0300

    add ParserGenerator.h docs

--- include/codegen/ParserGenerator.h
+++ include/codegen/ParserGenerator.h
@@ -1,23 +1,63 @@
+/**
+ * @file ParserGenerator.h
+ * @brief Provides a class for generating a parser from a given grammar, objects
+ * related to the parser and a few flags.
+ * @author Vadim Melnikov
+ * @version 1.0
+ */
 #pragma once
 
 #include "Entities.h"
 
+/**
+ * @class ParserGeneratorError
+ * @brief Exception class for reporting errors in the process of generating a
+ * parser.
+ */
 class ParserGeneratorError : public std::exception {
 public:
+    /**
+     * @brief Constructs a ParserGeneratorError object with the specified error.
+     */
     explicit ParserGeneratorError(const std::string &msg);
+
+    /**
+     * @brief Returns the error message.
+     * @return The error message.
+     */
     const char *what() const noexcept override;
 
 private:
     std::string msg_;
 };
 
+/**
+ * @class ParserGenerator
+ * @brief Generates a parser for the given grammar to a folder.
+ */
 class ParserGenerator {
 public:
+    /**
+     * @brief Constructs a ParserGenerator object with the specified grammar.
+     * @param folder The folder to generate the parser to.
+     * @param g The grammar to generate the parser for.
+     * @param at The action table to use for the generated parser.
+     * @param gt The goto table to use for the generated parser.
+     * @param fs The FOLLOW sets to use for the generated parser.
+     * @param add_json_generator Whether to add a JSON parse tree generator to
+     * the generated parser.
+     * @param json_indents The number of indents to use for the JSON parse tree
+     * (if it is generated).
+     */
     ParserGenerator(
         const std::string &folder, const Grammar &g, const ActionTable &at,
         const GotoTable &gt, const FollowSets &fs, bool add_json_generator,
         size_t json_indents
     );
+
+    /**
+     * @brief Generates the parser.
+     */
     void Generate();
 
 private:
